80e9a1b3830caacf4ea42581c56f3c3ccb22828e
WasabiRobby <79250053+WasabiRobby@users.noreply.github.com>
2022-06-10 01:57:12 -0700

    update: removed QBCore
    Pointless and not useful for QBCore

--- client/client.lua
+++ client/client.lua
@@ -1,7 +1,7 @@
 -----------------For support, scripts, and more----------------
 ----------------- https://discord.gg/XJFNyMy3Bv ---------------
 ---------------------------------------------------------------
-ESX, QBCore = nil, nil
+ESX = nil
 local sqlVehicles = {}
 local framework = ''
 
@@ -15,42 +15,19 @@ end)
 
 CreateThread(function()
     if Config.UseSQL then
-        if framework == '' then
-            if GetResourceState('es_extended') == "started" then
-                framework = 'esx'
-                while ESX == nil do 
-                    TriggerEvent("esx:getSharedObject", function(obj) 
-                        ESX = obj 
-                    end)
-                    Wait(500)
+        while ESX == nil do 
+            TriggerEvent("esx:getSharedObject", function(obj) 
+                ESX = obj 
+            end)
+            Wait(500)
+        end
+        ESX.TriggerServerCallback('wasabi_vehiclenames:getVehicles', function(cb)          
+            if cb then
+                for k,v in pairs(cb) do
+                    AddTextEntry(v.model, v.name)
                 end
-            elseif GetResourceState('qb-core') == "started" then
-                framework = 'qb'
-                QBCore = exports["qb-core"]:GetCoreObject()
             end
-        end
-    end
-end)
-
-CreateThread(function()
-    if Config.UseSQL and framework ~= '' then
-        if framework == 'esx' then
-            ESX.TriggerServerCallback('wasabi_vehiclenames:getVehicles', function(cb)          
-                if cb then
-                    for k,v in pairs(cb) do
-                        AddTextEntry(v.model, v.name)
-                    end
-                end
-            end)
-        elseif framework == 'qb' then
-            QBCore.Functions.TriggerCallback('wasabi_vehiclenames:getVehicles', function(cb)       
-                if cb then
-                    for k,v in pairs(cb) do
-                        AddTextEntry(v.model, v.name)
-                    end
-                end
-            end)
-        end
+        end)
     end
 end)
 
